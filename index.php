c6edae7c9595260cc4c0a73cc7f33e9bdecffcfb
Afia Afrin <99379464+AfiaAfrin@users.noreply.github.com>
2023-09-17 22:28:01 +0600

    Delete button

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
             <input type="text" name="num1" class="from-control" placeholder="Number 1">
             <input type="text" name="num2" class="from-control" placeholder="Number 2">
 
-            <input type="submit" value="Add" id="" >
-            <button></button>
+            <input type="submit" value="Add">
 </from>
 
 
