ad7e0db666600666789af6adcfd9eff70332c594
Afia Afrin <afiaafrinapon@gmail.com>
2023-09-17 12:33:52 +0600

    input text

---
+++ index.php
@@ -0,0 +1,9 @@
+ <from>
+            <input type="text" name="num1" class="from-control" placeholder="Number 1">
+            <input type="text" name="num2" class="from-control" placeholder="Number 2">
+
+            <input type="submit" value="Add" id="" >
+</from>
+
+
+     
